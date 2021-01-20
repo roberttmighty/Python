3306634918ecf4ee3b1bb631e2405dc5048a3386
roberttmighty <62709463+roberttmighty@users.noreply.github.com>
2021-01-20 08:42:19 -0500

    Update and rename s_CCLA_sftp.py to sftp_put_listdir.py

--- sftp_put_listdir.py
+++ sftp_put_listdir.py
@@ -1,34 +1,23 @@
 #!/usr/bin/python3
 
-"""
-Custom SPC Script to upload CCLA files.
-
-Last Update  : 06/23/2020 by Josh Westmoreland
-Changes Made : Remote directory list to date stamped file on /psdata/
-Next Steps   : Add email notification on completion if requested.
-
-"""
-
 #Import  modules
 import pysftp, paramiko, os, datetime
 
 #Variables
-PERSONNEL_F = "/psdata/ccla/personnel/perspc_20200817.dat"
-STUDENT_F = "/psdata/ccla/student/stuspc_05182020.dat"
+FILE_F = "/psdata/ccla/personnel/perspc_20200817.dat"
 OUTPUT_F = "/psdata/ccla/logs/ccla_upload_"
 TODAY = datetime.date.today().strftime("%Y%m%d")
 
 #SFTP Connection Variables
-r_HN = "securefiles.flvc.org"
-r_UN = "spftp"
-r_PW = "0PsU$er#3"
+r_HN = "hostname"
+r_UN = "username"
+r_PW = "password"
 
 #Define Functions
 def s_put(HN, UN, PW): #Connects to remote host via sftp and uploads files.
     with pysftp.Connection(host=HN, username=UN, password=PW) as sftp:
         _RDIR = sftp.listdir(r"/")
-        sftp.put(PERSONNEL_F, _RDIR)
-        sftp.put(STUDENT_F, _RDIR)
+        sftp.put(FILE_F, _RDIR)
 
 def s_list(HN, UN, PW): #Connects to remote hose via sftp and prints directory contents to file.
     with pysftp.Connection(host=HN, username=UN, password=PW) as sftp:
