b49762772b3516a4fed5b77c48c273576a2966f9
roberttmighty <62709463+roberttmighty@users.noreply.github.com>
2021-01-20 08:54:06 -0500

    Update and rename s_bn_adoption_portal_sftp.py to sftp_upload_filename_with_current_date.py

--- sftp_upload_filename_with_current_date.py
+++ sftp_upload_filename_with_current_date.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
 
-"""
-Custom SPC Script to upload the daily file to B&N Portal for processing and
-move .zip file to archive.
-
-Last Update  : 08/14/2020 by Josh Westmoreland
-Changes Made : Changed _RDIR variable to full remote path plus File.
-Next Steps   :
-
-"""
 import pysftp, paramiko, os, shutil, datetime
 
 #Variables
 TODAY =  datetime.date.today().strftime("%Y%m%d")
 YEAR = datetime.datetime.now().year
-SRC_FLDR = "/psdata/b_n/bn_portal/"
-FILE_NAME = TODAY + "_spcollege.bncroster.zip"
+SRC_FLDR = "source_directory"
+FILE_NAME = TODAY + "file_name"
 SRC_FILE = SRC_FLDR + FILE_NAME
 DST_FLDR = SRC_FLDR + "archive/%d/" % YEAR 
 
 #SFTP Connection Variables
-r_HN = "rex-sftp.bncollege.com"
-r_UN = "bnc8046"
-r_PK = "/oracle/PSAdmin_Files/scripts/app_servers/sftp/keys/bn_bookstore_portal_private.ppk"
-r_PKPW = "BNBookstorePortal0427"
+r_HN = "hostname"
+r_UN = "username"
+r_PK = "private_key"
+r_PKPW = "private_key_password"
 
 #Define Functions
 def s_create(folder): #Checks for destination spcifiec in argument and if not present creates it.
@@ -36,8 +27,8 @@ def s_move(): #Moves file from source folder to destination by name.
 
 def s_put(HN, UN, PK, PKPW): #Connects to remote host via sftp and uploads file.
     with pysftp.Connection(host=HN, username=UN, private_key=PK, private_key_pass=PKPW) as sftp:
-        sftp.cwd('/bned-sis-oneroster-mailbox-prod/data/IC8046/inbox/')
-        _RDIR = '/bned-sis-oneroster-mailbox-prod/data/IC8046/inbox/' + FILE_NAME
+        sftp.cwd('remote_directory')
+        _RDIR = 'remote_directory' + FILE_NAME
         sftp.put(SRC_FILE, _RDIR)
             
 
